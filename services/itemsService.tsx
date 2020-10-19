bcdc4a7d87cea2291d6996c73054b9f7e0d0ef1e
arjunhijam <arjun.hijam@samya.ai>
2020-10-19 13:29:45 +0530

    pagination on items api

--- services/itemsService.tsx
+++ services/itemsService.tsx
@@ -2,7 +2,7 @@ const fetchItems = async (Items, Price, req, res) => {
   let response = {};
   let items = [];
   let price = [];
-
+  let itemsCount = 0
   if (req.query.itemId) {
     await Items.get().then((priceSnapshot) => {
       // Fetches items based on item id
@@ -18,20 +18,38 @@ const fetchItems = async (Items, Price, req, res) => {
     let category = req.query.catId;
     let pageOffset = req.query.offset;
     console.log("check for page offset", pageOffset)
-    await Items.orderBy('lname').limit(37).offset(parseInt(pageOffset,10)).get().then((snapshot) => {
+    await Items.where('category','==',category).orderBy('lname').get().then((snapshot) => {
+      itemsCount = snapshot.docs.length;
+      snapshot.docs.forEach((doc) => {
+       // console.log("checking for items", doc.data());
+      //   response = doc.data();
+      //   response["_id"] = doc.id;
+      // //  console.log("this is response", response);
+      //   if (response["category"] === category) {
+      //     items.push(response);
+      //   }
+      });
+    });
+    await Items.where('category','==',category).orderBy('lname').limit(10).offset(parseInt(pageOffset,10)).get().then((snapshot) => {
       snapshot.docs.forEach((doc) => {
        // console.log("checking for items", doc.data());
         response = doc.data();
         response["_id"] = doc.id;
       //  console.log("this is response", response);
-        if (response["category"] === category) {
+       // if (response["category"] === category) {
           items.push(response);
-        }
+       // }
       });
     });
   }
  // console.log("to be sent", items);
-  return res.send(items);
+ response = {
+   items,
+   meta:{
+     count:itemsCount
+   }
+ }
+  return res.send(response);
 };
 
 exports.fetchItems = fetchItems;
