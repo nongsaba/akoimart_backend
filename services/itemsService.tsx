06554f78196c001a6aaf7ffdf8ad68f0db552816
arjunhijam <arjun.hijam@samya.ai>
2020-12-20 18:34:23 +0530

    items bug fix

--- services/itemsService.tsx
+++ services/itemsService.tsx
@@ -3,6 +3,8 @@ const fetchItems = async (Items, Price, req, res) => {
   let items = [];
   let price = [];
   let itemsCount = 0
+  console.log(req.query.discount )
+  console.log(req.query.catId )
   if (req.query.itemId) {
     await Items.get().then((priceSnapshot) => {
       // Fetches items based on item id
@@ -10,11 +12,14 @@ const fetchItems = async (Items, Price, req, res) => {
         if (doc.id == req.query.itemId) {
           response = doc.data();
           response["_id"] = doc.id;
-          items.push(response);
+          items.push(response); 
         }
       });
     });
-  } else if(req.query.catId && req.query.discount === undefined){
+  } 
+  // Executes when catId is there and discount is 0 or undefined
+
+  if(req.query.catId && req.query.discount <= 0 ){
     let category = req.query.catId;
     let pageOffset = req.query.offset;
     await Items.where('category','==',category).orderBy('lname').get().then((snapshot) => {
@@ -40,12 +45,16 @@ const fetchItems = async (Items, Price, req, res) => {
        // }
       });
     });
-  }else if(req.query.discount){
+  }
+  // Executes when catId is there and discount > 0 
+  if(req.query.discount > 0){
     // if cat ID is undefined
     let discount = req.query.discount
     // add category later
     await Items.get().then((snapshot) => {
+   
       snapshot.docs.forEach((doc) => {
+        itemsCount = snapshot.docs.length;
        // console.log("checking for items", doc.data());
         response = doc.data();
         response["_id"] = doc.id;
@@ -64,7 +73,7 @@ const fetchItems = async (Items, Price, req, res) => {
  response = {
    items,
    meta:{
-     count:items.length
+     count:itemsCount
    }
  }
   return res.send(response);
